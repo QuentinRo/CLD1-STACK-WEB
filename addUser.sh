a609bc6e9e7c6762c964baf0abeb9199d4e2784c
kevinjordil <kevin.jordil@cpnv.ch>
2017-11-02 09:19:08 +0100

    add pinguin

--- addUser.sh
+++ addUser.sh
@@ -8,6 +8,23 @@ cat <<TEXTBLOCK
  He create new user and the related config for his Web stack.
                   Follow the instructions !
 --------------------------------------------------------------
+							a8888b.
+							d888888b.
+							8P"YP"Y88
+							8|o||o|88
+							8'    .88
+							8`._.' Y8.
+						d/      `8b.
+						dP   .    Y8b.
+						d8:'  "  `::88b
+						d8"         'Y88b
+					:8P    '      :888
+						8a.   :     _a88P
+					._/"Yaa_:   .| 88P|
+					\    YP"    `| 8P  `.
+					/     \.___.d|    .'
+					`--..__)8888P`._.'
+--------------------------------------------------------------					
 TEXTBLOCK
 echo -e "\033[0m"
 
