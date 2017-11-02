76cf6088dbddaa9f3ede8d926b151e9100a5d400
Bastien <bastien.nicoud@gmail.com>
2017-11-02 09:28:24 +0100

    new maria db test

--- mariaDB_test.php
+++ mariaDB_test.php
@@ -1,17 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<h1>Enter your database identifiers</h1>
+<form action="mariaDB_test.php" method="POST">
+  <input type="text" name="username">
+  <input type="text" name="userpass">
+  <input type="text" name="userdb">
+  <input type="submit">
+</form>
+  
 <?php
 
-  if(empty($_POST['userdb'])){
-    ?>
-    <h1>Enter your database identifiers</h1>
-    <form action="mariaDB_test.php" method="POST">
-      <input type="text" name="username">
-      <input type="text" name="userpass">
-      <input type="text" name="userdb">
-
-      <input type="submit">
-    </form>
-    <?php
-  } else {
+  if($_POST['userdb'] && $_POST['username'] && $_POST['userpass']){
 
     $username = $_POST['username'];
     $userpass = $_POST['userpass'];
@@ -28,10 +35,20 @@
       $DB->exec('INSERT INTO `tutu` (`hello`) VALUES (`HEHEHEH this insert works well !`)');
 
       $stmt = $DB->query('SELECT * FROM tutu');
+
+      echo "DB insertion OK";
       print_r($stmt->fetchAll());
+
 
     } catch(PDOexeption $e) {
       return $e;
     }
 
+  } else {
+    echo "You didn't fill the fields correctly !";
   }
+
+?>
+
+</body>
+</html>
