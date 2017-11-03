4bb3aaa35319c9015482d56b2afeaa36676bce63
Bastien <bastien.nicoud@gmail.com>
2017-11-03 11:59:51 +0100

    indentation

--- mariaDB_test.php
+++ mariaDB_test.php
@@ -17,48 +17,48 @@
     <input type="submit">
   </form>
   
-<?php
+  <?php
 
-  // If the user have sent all the datas
-  if(isset($_POST['userdb']) && isset($_POST['username']) && isset($_POST['userpass'])){
+    // If the user have sent all the datas
+    if(isset($_POST['userdb']) && isset($_POST['username']) && isset($_POST['userpass'])){
 
-    // Gets the datas from post
-    $username = $_POST['username'];
-    $userpass = $_POST['userpass'];
-    $userdb = $_POST['userdb'];
+      // Gets the datas from post
+      $username = $_POST['username'];
+      $userpass = $_POST['userpass'];
+      $userdb = $_POST['userdb'];
 
-    try{
-      // Try a connexion to the DB
-      $DB = new PDO('mysql:dbname=' . $userdb . ';host=localhost', $username, $userpass);
+      try{
+        // Try a connexion to the DB
+        $DB = new PDO('mysql:dbname=' . $userdb . ';host=localhost', $username, $userpass);
 
-      // Create a table with 2 columns
-      $DB->exec('CREATE TABLE `tutu` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `hello` varchar(256) DEFAULT NULL,
-        PRIMARY KEY (id)
-      )');
+        // Create a table with 2 columns
+        $DB->exec('CREATE TABLE `tutu` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `hello` varchar(256) DEFAULT NULL,
+          PRIMARY KEY (id)
+        )');
 
-      // Insert data in the table
-      $DB->exec('INSERT INTO tutu (hello) VALUES ("He this insert works well !")');
+        // Insert data in the table
+        $DB->exec('INSERT INTO tutu (hello) VALUES ("He this insert works well !")');
 
-      // select this datas
-      $stmt = $DB->query('SELECT * FROM tutu');
+        // select this datas
+        $stmt = $DB->query('SELECT * FROM tutu');
 
-      echo "DB insertion OK";
-      print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
+        echo "DB insertion OK";
+        print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
 
 
-    } catch(PDOexeption $e) {
-      print_r($e);
+      } catch(Exeption $e) {
+        print_r($e);
+      }
+
+    } else {
+
+      // If the user have not send all the datas
+      echo "You didn't fill the fields correctly !";
     }
 
-  } else {
+  ?>
 
-    // If the user have not send all the datas
-    echo "You didn't fill the fields correctly !";
-  }
-
-?>
-
-</body>
+  </body>
 </html>
